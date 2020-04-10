258fed1a7c7d1291f89f9e226d29614571156cda
Ane Sanchiz <ane.sanchiz@opendeusto.es>
2020-04-10 19:35:17 +0200

    Clases

--- miProyecto/miApp/models.py
+++ miProyecto/miApp/models.py
@@ -1,25 +1,49 @@
 from django.db import models
 
 # Create your models here.
-class Departamento(models.Model):
-    nombre = models.CharField(max_length=50)
-    telefono = models.IntegerField()
+
+#CLASE PARA DEFINIR DATOS_CLIENTE EN LA CLASE PEDIDO
+class Cliente (models.Model):
+    CIF = models.CharField(max_length=9)
+    empresa = models.CharField(max_length=30)
+    telefono = models.CharField
+
+def __str__(self):
+        return f"CIF={self.CIF}, empresa={self.empresa}, telefono={self.telefono}"
+
+
+class Componente(models.Model):
+    codigo = models.CharField(max_length=10)
+    modelo = models.CharField(max_length=30)
+    marca = models.CharField(max_length=30)
 
     def __str__(self):
-        return f"id={self.id}, nombre={self.nombre}, telefono={self.telefono}"
+        return f"codigo={self.codigo}, nombre={self.modelo}, marca={self.marca}"
 
-class Habilidad(models.Model):
-    nombre = models.CharField(max_length=50)
 
-    def __str__(self):
-        return f"nombre={self.nombre}, antiguedad={self.antiguedad}"
-
-class Empleado(models.Model):
-    departamento = models.ForeignKey(Departamento, on_delete = models.CASCADE)
-    habilidades = models.ManyToManyField(Habilidad)
-    nombre= models.CharField(max_length = 50)
-    fecha_nacimiento = models.DateField()
-    antiguedad = models.IntegerField(default=0)
+class Productos(models.Model):
+    referencia = models.CharField(max_length=50)
+    precio = models.IntegerField
+    nombre = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=40) #DICCIONARIO DE CATEGORIAS(?
+    tipo_componentes = models.ManyToManyField(Componente)
 
     def __str__(self):
-        return f"id={self.id}, nombre={self.nombre}, fecha_nacimiento={self.fecha_nacimiento}, antiguedad={self.antiguedad}"
+        return f"referncia={self.nombre}, precio={self.precio}," \
+               f" nombre={self.nombre}, descripcion={self.descripcion}, " \
+               f"categoria={self.categoria}, componentes={self.tipo_componentes}"
+
+
+class Pedido(models.Model):
+    codigo = models.CharField #PRIMARY KEY (?)
+    fecha = models.DateField()
+    datos_cliente = models.ForeignKey(Cliente)
+    productos = models.ManyToManyField(Productos)
+    cantidad = models.IntegerField
+    precio_total = models.IntegerField
+
+    def __str__(self):
+        return f"codigo={self.codigo}, fecha={self.fecha}, datos cliente={self.datos_cliente}" \
+               f", productos={self.productos}, cantidad={self.cantidad}, precio={self.precio_total}"
+
